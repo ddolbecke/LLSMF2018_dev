0840c9bc34406d3bcb733c160880670d1169d617
jean <jean.cavillot@uclouvain.be>
2018-10-23 08:42:20 +0200

    Buzzer with files added to project

--- code/mrk-board/drivers/nbproject/Makefile-default.mk
+++ code/mrk-board/drivers/nbproject/Makefile-default.mk
@@ -19,7 +19,7 @@ endif
 endif
 
 # Environment
-MKDIR=mkdir -p
+MKDIR=gnumkdir -p
 RM=rm -f 
 MV=mv 
 CP=cp 
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=drivers/acl.c drivers/analogInputs.c drivers/ChrFont0.c drivers/ClpLib.c drivers/databk.c drivers/delay.c drivers/FillPat.c drivers/interrupts.c drivers/motors.c drivers/mx4.c drivers/OledChar.c drivers/OledGrph.c drivers/Pmod8leds.c drivers/PmodBTN.c drivers/PmodCLP.c drivers/PmodMAXSONAR.c drivers/PmodOLED.c drivers/PmodSSD.c drivers/PmodTMP3.c drivers/scope.c drivers/servo.c drivers/PmodLS1.c drivers/PmodIR_Range.c drivers/PmodJSTK.c sources/PmodKYPD.c sources/main.c
+SOURCEFILES_QUOTED_IF_SPACED=drivers/acl.c drivers/analogInputs.c drivers/ChrFont0.c drivers/ClpLib.c drivers/databk.c drivers/delay.c drivers/FillPat.c drivers/interrupts.c drivers/motors.c drivers/mx4.c drivers/OledChar.c drivers/OledGrph.c drivers/Pmod8leds.c drivers/PmodBTN.c drivers/PmodCLP.c drivers/PmodMAXSONAR.c drivers/PmodOLED.c drivers/PmodSSD.c drivers/PmodTMP3.c drivers/scope.c drivers/servo.c drivers/PmodLS1.c drivers/PmodIR_Range.c drivers/PmodJSTK.c sources/PmodKYPD.c sources/main.c drivers/PmodBuzzer.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/drivers/acl.o ${OBJECTDIR}/drivers/analogInputs.o ${OBJECTDIR}/drivers/ChrFont0.o ${OBJECTDIR}/drivers/ClpLib.o ${OBJECTDIR}/drivers/databk.o ${OBJECTDIR}/drivers/delay.o ${OBJECTDIR}/drivers/FillPat.o ${OBJECTDIR}/drivers/interrupts.o ${OBJECTDIR}/drivers/motors.o ${OBJECTDIR}/drivers/mx4.o ${OBJECTDIR}/drivers/OledChar.o ${OBJECTDIR}/drivers/OledGrph.o ${OBJECTDIR}/drivers/Pmod8leds.o ${OBJECTDIR}/drivers/PmodBTN.o ${OBJECTDIR}/drivers/PmodCLP.o ${OBJECTDIR}/drivers/PmodMAXSONAR.o ${OBJECTDIR}/drivers/PmodOLED.o ${OBJECTDIR}/drivers/PmodSSD.o ${OBJECTDIR}/drivers/PmodTMP3.o ${OBJECTDIR}/drivers/scope.o ${OBJECTDIR}/drivers/servo.o ${OBJECTDIR}/drivers/PmodLS1.o ${OBJECTDIR}/drivers/PmodIR_Range.o ${OBJECTDIR}/drivers/PmodJSTK.o ${OBJECTDIR}/sources/PmodKYPD.o ${OBJECTDIR}/sources/main.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/drivers/acl.o.d ${OBJECTDIR}/drivers/analogInputs.o.d ${OBJECTDIR}/drivers/ChrFont0.o.d ${OBJECTDIR}/drivers/ClpLib.o.d ${OBJECTDIR}/drivers/databk.o.d ${OBJECTDIR}/drivers/delay.o.d ${OBJECTDIR}/drivers/FillPat.o.d ${OBJECTDIR}/drivers/interrupts.o.d ${OBJECTDIR}/drivers/motors.o.d ${OBJECTDIR}/drivers/mx4.o.d ${OBJECTDIR}/drivers/OledChar.o.d ${OBJECTDIR}/drivers/OledGrph.o.d ${OBJECTDIR}/drivers/Pmod8leds.o.d ${OBJECTDIR}/drivers/PmodBTN.o.d ${OBJECTDIR}/drivers/PmodCLP.o.d ${OBJECTDIR}/drivers/PmodMAXSONAR.o.d ${OBJECTDIR}/drivers/PmodOLED.o.d ${OBJECTDIR}/drivers/PmodSSD.o.d ${OBJECTDIR}/drivers/PmodTMP3.o.d ${OBJECTDIR}/drivers/scope.o.d ${OBJECTDIR}/drivers/servo.o.d ${OBJECTDIR}/drivers/PmodLS1.o.d ${OBJECTDIR}/drivers/PmodIR_Range.o.d ${OBJECTDIR}/drivers/PmodJSTK.o.d ${OBJECTDIR}/sources/PmodKYPD.o.d ${OBJECTDIR}/sources/main.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/drivers/acl.o ${OBJECTDIR}/drivers/analogInputs.o ${OBJECTDIR}/drivers/ChrFont0.o ${OBJECTDIR}/drivers/ClpLib.o ${OBJECTDIR}/drivers/databk.o ${OBJECTDIR}/drivers/delay.o ${OBJECTDIR}/drivers/FillPat.o ${OBJECTDIR}/drivers/interrupts.o ${OBJECTDIR}/drivers/motors.o ${OBJECTDIR}/drivers/mx4.o ${OBJECTDIR}/drivers/OledChar.o ${OBJECTDIR}/drivers/OledGrph.o ${OBJECTDIR}/drivers/Pmod8leds.o ${OBJECTDIR}/drivers/PmodBTN.o ${OBJECTDIR}/drivers/PmodCLP.o ${OBJECTDIR}/drivers/PmodMAXSONAR.o ${OBJECTDIR}/drivers/PmodOLED.o ${OBJECTDIR}/drivers/PmodSSD.o ${OBJECTDIR}/drivers/PmodTMP3.o ${OBJECTDIR}/drivers/scope.o ${OBJECTDIR}/drivers/servo.o ${OBJECTDIR}/drivers/PmodLS1.o ${OBJECTDIR}/drivers/PmodIR_Range.o ${OBJECTDIR}/drivers/PmodJSTK.o ${OBJECTDIR}/sources/PmodKYPD.o ${OBJECTDIR}/sources/main.o ${OBJECTDIR}/drivers/PmodBuzzer.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/drivers/acl.o.d ${OBJECTDIR}/drivers/analogInputs.o.d ${OBJECTDIR}/drivers/ChrFont0.o.d ${OBJECTDIR}/drivers/ClpLib.o.d ${OBJECTDIR}/drivers/databk.o.d ${OBJECTDIR}/drivers/delay.o.d ${OBJECTDIR}/drivers/FillPat.o.d ${OBJECTDIR}/drivers/interrupts.o.d ${OBJECTDIR}/drivers/motors.o.d ${OBJECTDIR}/drivers/mx4.o.d ${OBJECTDIR}/drivers/OledChar.o.d ${OBJECTDIR}/drivers/OledGrph.o.d ${OBJECTDIR}/drivers/Pmod8leds.o.d ${OBJECTDIR}/drivers/PmodBTN.o.d ${OBJECTDIR}/drivers/PmodCLP.o.d ${OBJECTDIR}/drivers/PmodMAXSONAR.o.d ${OBJECTDIR}/drivers/PmodOLED.o.d ${OBJECTDIR}/drivers/PmodSSD.o.d ${OBJECTDIR}/drivers/PmodTMP3.o.d ${OBJECTDIR}/drivers/scope.o.d ${OBJECTDIR}/drivers/servo.o.d ${OBJECTDIR}/drivers/PmodLS1.o.d ${OBJECTDIR}/drivers/PmodIR_Range.o.d ${OBJECTDIR}/drivers/PmodJSTK.o.d ${OBJECTDIR}/sources/PmodKYPD.o.d ${OBJECTDIR}/sources/main.o.d ${OBJECTDIR}/drivers/PmodBuzzer.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/drivers/acl.o ${OBJECTDIR}/drivers/analogInputs.o ${OBJECTDIR}/drivers/ChrFont0.o ${OBJECTDIR}/drivers/ClpLib.o ${OBJECTDIR}/drivers/databk.o ${OBJECTDIR}/drivers/delay.o ${OBJECTDIR}/drivers/FillPat.o ${OBJECTDIR}/drivers/interrupts.o ${OBJECTDIR}/drivers/motors.o ${OBJECTDIR}/drivers/mx4.o ${OBJECTDIR}/drivers/OledChar.o ${OBJECTDIR}/drivers/OledGrph.o ${OBJECTDIR}/drivers/Pmod8leds.o ${OBJECTDIR}/drivers/PmodBTN.o ${OBJECTDIR}/drivers/PmodCLP.o ${OBJECTDIR}/drivers/PmodMAXSONAR.o ${OBJECTDIR}/drivers/PmodOLED.o ${OBJECTDIR}/drivers/PmodSSD.o ${OBJECTDIR}/drivers/PmodTMP3.o ${OBJECTDIR}/drivers/scope.o ${OBJECTDIR}/drivers/servo.o ${OBJECTDIR}/drivers/PmodLS1.o ${OBJECTDIR}/drivers/PmodIR_Range.o ${OBJECTDIR}/drivers/PmodJSTK.o ${OBJECTDIR}/sources/PmodKYPD.o ${OBJECTDIR}/sources/main.o
+OBJECTFILES=${OBJECTDIR}/drivers/acl.o ${OBJECTDIR}/drivers/analogInputs.o ${OBJECTDIR}/drivers/ChrFont0.o ${OBJECTDIR}/drivers/ClpLib.o ${OBJECTDIR}/drivers/databk.o ${OBJECTDIR}/drivers/delay.o ${OBJECTDIR}/drivers/FillPat.o ${OBJECTDIR}/drivers/interrupts.o ${OBJECTDIR}/drivers/motors.o ${OBJECTDIR}/drivers/mx4.o ${OBJECTDIR}/drivers/OledChar.o ${OBJECTDIR}/drivers/OledGrph.o ${OBJECTDIR}/drivers/Pmod8leds.o ${OBJECTDIR}/drivers/PmodBTN.o ${OBJECTDIR}/drivers/PmodCLP.o ${OBJECTDIR}/drivers/PmodMAXSONAR.o ${OBJECTDIR}/drivers/PmodOLED.o ${OBJECTDIR}/drivers/PmodSSD.o ${OBJECTDIR}/drivers/PmodTMP3.o ${OBJECTDIR}/drivers/scope.o ${OBJECTDIR}/drivers/servo.o ${OBJECTDIR}/drivers/PmodLS1.o ${OBJECTDIR}/drivers/PmodIR_Range.o ${OBJECTDIR}/drivers/PmodJSTK.o ${OBJECTDIR}/sources/PmodKYPD.o ${OBJECTDIR}/sources/main.o ${OBJECTDIR}/drivers/PmodBuzzer.o
 
 # Source Files
-SOURCEFILES=drivers/acl.c drivers/analogInputs.c drivers/ChrFont0.c drivers/ClpLib.c drivers/databk.c drivers/delay.c drivers/FillPat.c drivers/interrupts.c drivers/motors.c drivers/mx4.c drivers/OledChar.c drivers/OledGrph.c drivers/Pmod8leds.c drivers/PmodBTN.c drivers/PmodCLP.c drivers/PmodMAXSONAR.c drivers/PmodOLED.c drivers/PmodSSD.c drivers/PmodTMP3.c drivers/scope.c drivers/servo.c drivers/PmodLS1.c drivers/PmodIR_Range.c drivers/PmodJSTK.c sources/PmodKYPD.c sources/main.c
+SOURCEFILES=drivers/acl.c drivers/analogInputs.c drivers/ChrFont0.c drivers/ClpLib.c drivers/databk.c drivers/delay.c drivers/FillPat.c drivers/interrupts.c drivers/motors.c drivers/mx4.c drivers/OledChar.c drivers/OledGrph.c drivers/Pmod8leds.c drivers/PmodBTN.c drivers/PmodCLP.c drivers/PmodMAXSONAR.c drivers/PmodOLED.c drivers/PmodSSD.c drivers/PmodTMP3.c drivers/scope.c drivers/servo.c drivers/PmodLS1.c drivers/PmodIR_Range.c drivers/PmodJSTK.c sources/PmodKYPD.c sources/main.c drivers/PmodBuzzer.c
 
 
 CFLAGS=
@@ -262,6 +262,12 @@ ${OBJECTDIR}/sources/main.o: sources/main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/sources/main.o 
 	@${FIXDEPS} "${OBJECTDIR}/sources/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -I"sources" -I"drivers" -I"plib/pic32mx/include" -MMD -MF "${OBJECTDIR}/sources/main.o.d" -o ${OBJECTDIR}/sources/main.o sources/main.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
 	
+${OBJECTDIR}/drivers/PmodBuzzer.o: drivers/PmodBuzzer.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/drivers" 
+	@${RM} ${OBJECTDIR}/drivers/PmodBuzzer.o.d 
+	@${RM} ${OBJECTDIR}/drivers/PmodBuzzer.o 
+	@${FIXDEPS} "${OBJECTDIR}/drivers/PmodBuzzer.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -I"sources" -I"drivers" -I"plib/pic32mx/include" -MMD -MF "${OBJECTDIR}/drivers/PmodBuzzer.o.d" -o ${OBJECTDIR}/drivers/PmodBuzzer.o drivers/PmodBuzzer.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
+	
 else
 ${OBJECTDIR}/drivers/acl.o: drivers/acl.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/drivers" 
@@ -419,6 +425,12 @@ ${OBJECTDIR}/sources/main.o: sources/main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/sources/main.o 
 	@${FIXDEPS} "${OBJECTDIR}/sources/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -I"sources" -I"drivers" -I"plib/pic32mx/include" -MMD -MF "${OBJECTDIR}/sources/main.o.d" -o ${OBJECTDIR}/sources/main.o sources/main.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
 	
+${OBJECTDIR}/drivers/PmodBuzzer.o: drivers/PmodBuzzer.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/drivers" 
+	@${RM} ${OBJECTDIR}/drivers/PmodBuzzer.o.d 
+	@${RM} ${OBJECTDIR}/drivers/PmodBuzzer.o 
+	@${FIXDEPS} "${OBJECTDIR}/drivers/PmodBuzzer.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -I"sources" -I"drivers" -I"plib/pic32mx/include" -MMD -MF "${OBJECTDIR}/drivers/PmodBuzzer.o.d" -o ${OBJECTDIR}/drivers/PmodBuzzer.o drivers/PmodBuzzer.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
+	
 endif
 
 # ------------------------------------------------------------------------------------
@@ -432,13 +444,13 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/drivers.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  ./plib/pic32mx/lib/libmchp_peripheral_32MX460F512L.a ./plib/pic32mx/lib/libmchp_peripheral.a  
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/drivers.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}    ./plib/pic32mx/lib/libmchp_peripheral_32MX460F512L.a ./plib/pic32mx/lib/libmchp_peripheral.a      -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD)   -mreserve=data@0x0:0x1FC -mreserve=boot@0x1FC02000:0x1FC02FEF -mreserve=boot@0x1FC02000:0x1FC024FF  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_PK3=1,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml
+	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/drivers.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}    .\plib\pic32mx\lib\libmchp_peripheral_32MX460F512L.a .\plib\pic32mx\lib\libmchp_peripheral.a      -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD)   -mreserve=data@0x0:0x1FC -mreserve=boot@0x1FC02000:0x1FC02FEF -mreserve=boot@0x1FC02000:0x1FC024FF  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_PK3=1,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml
 	
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/drivers.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  ./plib/pic32mx/lib/libmchp_peripheral_32MX460F512L.a ./plib/pic32mx/lib/libmchp_peripheral.a 
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/drivers.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}    ./plib/pic32mx/lib/libmchp_peripheral_32MX460F512L.a ./plib/pic32mx/lib/libmchp_peripheral.a      -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD)  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml
-	${MP_CC_DIR}/xc32-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/drivers.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/drivers.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}    .\plib\pic32mx\lib\libmchp_peripheral_32MX460F512L.a .\plib\pic32mx\lib\libmchp_peripheral.a      -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD)  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml
+	${MP_CC_DIR}\\xc32-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/drivers.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} 
 endif
 
 
@@ -457,7 +469,7 @@ endif
 # Enable dependency checking
 .dep.inc: .depcheck-impl
 
-DEPFILES=$(shell "${PATH_TO_IDE_BIN}"mplabwildcard ${POSSIBLE_DEPFILES})
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
 ifneq (${DEPFILES},)
 include ${DEPFILES}
 endif
